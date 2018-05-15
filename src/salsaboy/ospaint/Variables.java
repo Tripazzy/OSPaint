25dc8368a73a8e527925d46c34538f765d951a75
lpasfiel <lpasfiel@as.edu.au>
2018-05-15 10:18:15 +1000

    Created some basic files.

---
+++ src/salsaboy/ospaint/Variables.java
@@ -0,0 +1,7 @@
+package salsaboy.ospaint;
+
+import javax.swing.*;
+
+public class Variables {
+    public static JFrame frame = new JFrame();
+}
---
+++ src/salsaboy/ospaint/ui/MainMenu.java
@@ -0,0 +1,11 @@
+package salsaboy.ospaint.ui;
+
+import salsaboy.ospaint.Variables;
+
+public class MainMenu {
+    public MainMenu() {
+        Variables.frame.setLayout(null);
+        
+        Variables.frame.setSize(600, 371);  //Odd number from Golden Ratio. Don't judge.
+    }
+}
