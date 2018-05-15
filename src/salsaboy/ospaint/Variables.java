174bc55eb06770eca1e331c61052ec079f831e30
lpasfiel <lpasfiel@as.edu.au>
2018-05-15 10:29:14 +1000

    Centered the window for the menu.

--- src/salsaboy/ospaint/Variables.java
+++ src/salsaboy/ospaint/Variables.java
@@ -1,7 +1,11 @@
 package salsaboy.ospaint;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Variables {
     public static JFrame frame = new JFrame();
+    public static Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    public static int screenWidth = (int) (screenSize.getWidth());
+    public static int screenHeight = (int) (screenSize.getHeight());
 }
--- src/salsaboy/ospaint/ui/MainMenu.java
+++ src/salsaboy/ospaint/ui/MainMenu.java
@@ -1,11 +1,17 @@
 package salsaboy.ospaint.ui;
 
 import salsaboy.ospaint.Variables;
+import javax.swing.*;
 
 public class MainMenu {
     public MainMenu() {
         Variables.frame.setLayout(null);
         
         Variables.frame.setSize(600, 371);  //Odd number from Golden Ratio. Don't judge.
+        Variables.frame.setLocation(Variables.screenWidth / 2 - 300, Variables.screenHeight / 2 - 185);
+        Variables.frame.setResizable(false);
+        Variables.frame.setUndecorated(true);
+        Variables.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Variables.frame.setVisible(true);
     }
 }
